d913055ff8dc7e0b80455b189ed82bb529ddd9da
Ulrich Hetmaniuk <ulrich.hetmaniuk@gmail.com>
2020-07-09 16:56:09 -0700

    Update

--- uh_code.cpp
+++ uh_code.cpp
@@ -9,21 +9,21 @@ int main(int argc, char **argv) {
 
 #ifdef HAVE_MPI
   MPI_Init(&argc, &argv);
-#end
+#endif
 
   int myRank = 0, numRank = 1;
  
 #ifdef HAVE_MPI
   MPI_Comm_size(MPI_COMM_WORLD, &numRank);
   MPI_Comm_rank(MPI_COMM_WORLD, &myRank);
-#end
+#endif
 
 std::cout << " Code running on " << numRank << " procs ... I am " << myRank << "\n";
   
 #ifdef HAVE_MPI
   std::cout << " Call to MPI_Finalize()\n";
   MPI_Finalize();
-#end
+#endif
 
 return 0;
 
